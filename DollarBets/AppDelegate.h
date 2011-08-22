67e48a40391325364cc52a5a71579e8e7de28dd6
Richard Kirk <RichardBKirk@gmail.com>
2011-08-21 20:20:47 -0500

    Initial Commit

---
+++ DollarBets/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DollarBets
+//
+//  Created by Richard Kirk on 8/21/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DollarBets/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DollarBets
+//
+//  Created by Richard Kirk on 8/21/11.
+//  Copyright (c) 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
